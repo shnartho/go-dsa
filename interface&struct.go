d5cf17a71adf9c03a9e3bcc0c5fb1d1fb221d2ee
Md Shahadat Hossen Nayem <shnartho@gmail.com>
2023-05-01 23:31:48 +0100

    interface and struct problem

---
+++ interface&struct.go
@@ -0,0 +1,33 @@
+package main
+
+import "math"
+
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.width + r.height)
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
--- main.go
+++ main.go
@@ -3,6 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	testArr := []int{3, 2, -5, -1, 5}
-	fmt.Println(MaxSubArraySum(testArr))
+	var newShape Shape
+
+	newShape = Rectangle{width: 5, height: 4}
+	fmt.Println("Area of rectangle: ", newShape.Area())
+	fmt.Println("Perimeter of rectangle: ", newShape.Perimeter())
+
+	newShape = Circle{radius: 4}
+	fmt.Println("Area of circle: ", newShape.Area())
+	fmt.Println("Perimeter of circle: ", newShape.Perimeter())
 }
+
+// func main() {
+// 	testArr := []int{3, 2, -5, -1, 5}
+// 	fmt.Println(MaxSubArraySum(testArr))
+// }
+
+// func main() {
+// 	mystring := "helehq"
+// 	print(palindromeChecker(mystring))
+
+// }

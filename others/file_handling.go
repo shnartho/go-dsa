2b8394a546aa6438484b3ac49b867d1c3af25bf9
shnartho <shnartho@gmail.com>
2023-08-27 00:18:20 +0100

    others updated

--- others/file_handling.go
+++ others/file_handling.go
@@ -24,4 +24,33 @@ func main() {
 	}
 	fmt.Println("File content:", string(readContent))
 
+	// append in file
+	appendContent := []byte("\nAppending more content in the same file")
+	file, appendErr := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND, 0644)
+	if appendErr != nil {
+		fmt.Println("Error opening file:", appendErr)
+		return
+	}
+	defer file.Close()
+
+	_, writeErr := file.Write(appendContent)
+	if writeErr != nil {
+		fmt.Println("Error appending to file", writeErr)
+		return
+	}
+	fmt.Printf("Successfully append %s to %s", string(appendContent), filename)
+
+	// Read the contents after appending
+	appendedContent, _ := os.ReadFile(filename)
+	fmt.Println("Content: ", string(appendedContent))
+
+	// openfile manually and close
+	openFile, _ := os.Open(filename)
+	defer file.Close()
+
+	// read from open file
+	buf := make([]byte, 200)
+	n, _ := openFile.Read(buf)
+	fmt.Println("Read from the opened file: ", string(buf[:n]))
+
 }
